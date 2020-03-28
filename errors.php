ba20c4858ed6aed536d4df541a4e0c2315f41346
tholeb <thomasleb.49@gmail.com>
2020-03-28 12:07:00 +0100

    fix 404 image not loading on 404 error

--- errors.php
+++ errors.php
@@ -37,7 +37,7 @@
               <h3 class="white-text center"> Error 404: Page Not Found</h3> <br> <br>
             </div>
             <div class="row">
-              <p class="center"><img src="assets/img/errors/404.png" alt="404"></p> <br> <br>
+              <p class="center"><img src="<?= $wURL ?>assets/img/errors/404.png" alt="404"></p> <br> <br>
             </div>
             <div class="row">
               <h6 class="white-text center"> Sorry, the page you're looking for cannot be accessed or doesn't exits. <br> Either check the URL, <a href="<?= $wURL ?>" class="teal-text text-lighten-2">go home <i class="fas fa-home"></i></a>, or feel free to <a href="mailto:tholeb.pro@gmail.com" class="teal-text text-lighten-2">report this issue <i class="fas fa-bug"></i></a>.</h6>
@@ -79,7 +79,7 @@
               <h6 class="white-text center"> Are you trying to break the website ? that's not very nice of you :'(</h6>
             </div>
           </div>
-          <audio autoplay>
+          <audio autoplay controls style="display:none;">
             <source src="https://www.myinstants.com/media/sounds/stop_ZnQ9jIO.mp3" type="audio/mp3">
             Your browser does not support the audio element.
           </audio>
